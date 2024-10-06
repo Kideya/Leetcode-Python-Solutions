ec3deeea68be4d23e62fef89706bfce88dae54d8
Kideya <officialfelixkranz@gmx.de>
2024-10-07 00:15:46 +0200

    Refactor and added code for: - Iterating thorugh indexes (Nesting for loops) - checking for matching pairs in relation to target - added dictionary - checking thorugh dictionary how many pairs there are - printing all the info to the console (the pairs and the iterations) -pritning the dict to the console

--- Brute_force_cheking.py
+++ Brute_force_cheking.py
@@ -1,14 +1,12 @@
+# List that contains integers
+nums = [11,4,2,7,11,15]
 
-#List that contains integers
-#nums = [2,7,11,15]
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-#the target we want to get of the sum of two integers in num
-target = 9 
+# the target we want to get of the sum of two integers in num
+target = 15
 
 previousNumSize = None
 
-
-def Check_for_greater(nums,target):
+def Check_for_greater(nums, target):
     while True:
         for x in nums:
             currentNumSize = len(nums)
@@ -16,28 +14,29 @@ def Check_for_greater(nums,target):
                 nums.remove(max(nums))
             else:
                 return nums
-            
-def Conten_equals_Target(nums,target):
+
+def Conten_equals_Target(nums, target):
     index = 0
     firstInt = nums[index]
-    checkedPairs = 0
-    amountOfPairs = 0
-    
-    for x in nums:
-        if firstInt + x == target:
-            print(f"Numbers which sum equals {target} are {firstInt} and {x}, total number of pairs in the List are {amountOfPairs} \n" 
-                  f"amount of checked pairs are {checkedPairs}")
-            amountOfPairs += 1
-            index += 1
-            firstInt 
-        
-        else:
-            checkedPairs += 1
-        
-           
-       
-            
+    iterations = 0
+    pairs = {}
+    for x in range(len(nums)):
+        firstInt = nums[x]
+        for y in range(x + 1, len(nums)):
+            secondInt = nums[y]
+            if firstInt + secondInt == target:
+                iterations += 1
 
-    
-Check_for_greater(nums,target)
-Conten_equals_Target(nums,target)
+                pairs.update({firstInt: secondInt})
+
+                print(
+                    f"The pair {firstInt} and {secondInt} are equal to {target}\n"
+                    f"it took {iterations} iterations to find this pair and theire are a total of {len(pairs)} pairs \n"
+                )
+
+                print(f"{pairs}\n")
+            else:
+                iterations += 1
+
+Check_for_greater(nums, target)
+Conten_equals_Target(nums, target)
