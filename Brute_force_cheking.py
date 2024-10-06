fc5b08d9419bc41cbe3855319db069c655b1ea4d
Kideya <officialfelixkranz@gmx.de>
2024-10-07 00:17:01 +0200

    Removed unused variables

--- Brute_force_cheking.py
+++ Brute_force_cheking.py
@@ -4,12 +4,9 @@ nums = [11,4,2,7,11,15]
 # the target we want to get of the sum of two integers in num
 target = 15
 
-previousNumSize = None
-
 def Check_for_greater(nums, target):
     while True:
         for x in nums:
-            currentNumSize = len(nums)
             if max(nums) > target:
                 nums.remove(max(nums))
             else:
