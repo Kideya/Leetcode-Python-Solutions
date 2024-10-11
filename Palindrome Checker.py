2faa2defca995349680cc0f608c0dfaba089ac45
Kideya <officialfelixkranz@gmx.de>
2024-10-11 19:56:53 +0200

    Added Function for slicing up the Number

--- Palindrome Checker.py
+++ Palindrome Checker.py
@@ -1,18 +1,44 @@
-target = 1
+import math
+target = 120032940
 isPalindrome = None
+slicedTarget = []
 
-def SliceTarget(target):
+
+def CreateListLength(target):
     potence = 1
     x = 0
+    
     while x  <= target:
         x = pow(10,potence)
         potence += 1
-        print(x)
-    if x >= target and target < 999:
+        
+    if x >= target:
         potence -= 1
+        listLength = potence 
         print(f"The Number has {potence} digits")
-    elif x>= target:
-        potence += 1
-        print(f"The Number has {potence} digits")
+        
+    while listLength > 0:
+        slicedTarget.append("")
+        listLength -= 1
+    
+    if listLength == 0:
+        return (potence -1)
 
-SliceTarget(target)
+def sliceNumber(slicedTarget,target,potence):
+    index = 0
+    while target > 10:
+        FirstTarget = target / pow(10,potence)
+        FirstTarget = math.floor(FirstTarget)
+        target -= FirstTarget * pow(10,potence)
+        potence -= 1
+        slicedTarget[index] = FirstTarget
+        index += 1
+        print(FirstTarget,slicedTarget) 
+    
+    for x in range(len(slicedTarget)):
+        if isinstance(slicedTarget[x],str):
+            slicedTarget[x] = 0
+            print(slicedTarget)
+        
+    
+sliceNumber(slicedTarget,target,CreateListLength(target))
