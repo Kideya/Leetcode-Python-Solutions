0611b92286e37f8243a27f3e08b14401d4635f42
Kideya <officialfelixkranz@gmx.de>
2024-10-27 18:03:03 +0100

    added solution

---
+++ 2.AddTwoNumbers.py
@@ -0,0 +1,9 @@
+l1 = [2,4,3]
+l2 = [5,6,4]
+
+def addTwoNumbers(l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
