1f1e18b61aed554ea1bdf4194c04cd1fd1c8e07b
Kideya <officialfelixkranz@gmx.de>
2024-10-27 17:37:41 +0100

    Added Function -convert Roman Numbers to Integer

--- RomanToInteger.py
+++ RomanToInteger.py
@@ -1,5 +1,19 @@
 s = "III"
-class Solution(object):
-    def romanToInt(self, s):
-        splitRoman = list(s)
-        print(splitRoman)
+def RomanToInteger(s):
+    conversions = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    splitRoman = list(s)
+    currentValue = 0
+    skipNumber = False
+    
+    for i in range(len(splitRoman)):
+            if i + 1 < len(splitRoman) and conversions[splitRoman[i]] < conversions[splitRoman[i + 1]] and not skipNumber:
+                currentValue += (conversions[splitRoman[i + 1]] - conversions[splitRoman[i]])
+                skipNumber = True
+            elif skipNumber:
+                skipNumber = False
+            else:
+                currentValue += conversions[splitRoman[i]]
+    print(currentValue)    
+    return currentValue
+            
+RomanToInteger(s)
